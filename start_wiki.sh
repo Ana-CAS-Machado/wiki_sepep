38524e3194226ebabe4410e2500c22e3fba5a018
Ana Carolina <121954394+Ana-CAS-Machado@users.noreply.github.com>
2023-03-22 10:17:21 -0300

    script para rodar a imagem do docker da wiki

---
+++ start_wiki.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+version="2.0"
+container_name="sepep-mediawiki_v$version"
+imagem_media_wiki="mediawiki"
+porta_aberta=80
+proj_name="wiki_sepep"
+
+docker run --name $container_name -v /home/$proj_name:/var/local -p $porta_aberta:80 -d $imagem_media_wiki
