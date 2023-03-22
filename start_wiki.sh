58770617b5fef70ba2a7b3b13d7c614a5faf7aaa
Ana Carolina <121954394+Ana-CAS-Machado@users.noreply.github.com>
2023-03-22 10:56:38 -0300

    arrumando docker exec

--- start_wiki.sh
+++ start_wiki.sh
@@ -9,8 +9,7 @@ proj_name="wiki_sepep"
 docker run --name $container_name -v /home/$proj_name:/var/local -p $porta_aberta:80 -d $imagem_media_wiki
 docker cp LocalSettings.php $container_name:/var/www/html
 
-docker exec -it $container_name bash
-apt-get upgrade
-apt-get install sqlite3
-
-sqlite3 sepep_wiki.sqlite .dump > bkp_sepep_wiki.sql
+docker exec $container_name apt-get update
+docker exec $container_name apt-get upgrade
+docker exec $container_name apt-get install sqlite3
+docker exec $container_name sqlite3 sepep_wiki.sqlite .dump > bkp_sepep_wiki.sql
