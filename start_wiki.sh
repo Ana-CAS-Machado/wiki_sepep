93b51568c9ff3be15a1d3727c4132f08c3a1e1b2
Ana Carolina <121954394+Ana-CAS-Machado@users.noreply.github.com>
2023-03-22 11:14:05 -0300

    remove cp localsettings
    soh pode cp localsettings depois de configurar a wiki pela interface

--- start_wiki.sh
+++ start_wiki.sh
@@ -7,7 +7,6 @@ porta_aberta=80
 proj_name="wiki_sepep"
 
 docker run --name $container_name -v /home/$proj_name:/var/local -p $porta_aberta:80 -d $imagem_media_wiki
-docker cp LocalSettings.php $container_name:/var/www/html
 
 docker exec $container_name apt-get update
 docker exec $container_name apt-get upgrade
